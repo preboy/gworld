58314614dcfc9be868336e9c1af421fcc09453fa
preboy.sl <preboy@126.com>
2017-11-14 11:49:22 +0800

    * code tidy

---
+++ src/core/utils/signal.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func RegisterSignalHandler(f func(os.Signal)) {
+	go func() {
+		log.Info("register signal complete")
+
+		c := make(chan os.Signal, 10)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
+		for s := range c {
+			f(s)
+			if s != syscall.SIGHUP {
+				break
+			}
+		}
+	}()
+}
