58ed2dc727fdb4e16888628b9e09f701857e3d21
preboy.sl <preboy@126.com>
2018-01-03 13:17:41 +0800

    * exporter more fluency

---
+++ plan/readme.txt
@@ -0,0 +1,4 @@
+每一个excel文件只导出第一个sheet
+第一个sheet的名字就是导出之后的文件名
+类型: string number map array
+示例：hero.xlsx
--- src/tools/exporter/main.go
+++ src/tools/exporter/main.go
@@ -10,12 +10,146 @@ import (
 	"sync"
 )
 
-func expand_text_lua(filed, text string) string {
-	return text
+// field := "Rewards#ItemID_ItemCt_Type"
+// text := "2001_9_1|2007_3_2|2009_3_6"
+func expand_json_map(field, text string) (string, string) {
+	text = strings.Trim(text, " ")
+
+	idx := strings.Index(field, "#")
+	if idx == -1 {
+		return field, text
+	}
+
+	field_split := strings.Split(field, "#")
+	key := field_split[0]
+	sub_field := field_split[1]
+
+	if len(text) == 0 {
+		return key, "[]"
+	}
+
+	str := "[\n"
+
+	fields := strings.Split(sub_field, "_")
+
+	values := strings.Split(text, "|")
+	for i, value := range values {
+		sub_values := strings.Split(value, "_")
+		if len(sub_values) != len(fields) {
+			panic("expand_json_map error:" + field + " & " + text)
+		}
+		s := "\t\t\t{\n"
+		for k, sub_value := range sub_values {
+			s = s + "\t\t\t\t\"" + fields[k] + "\" : " + sub_value
+			if k != len(sub_values)-1 {
+				s = s + ",\n"
+			} else {
+				s = s + "\n"
+			}
+		}
+		if i != len(values)-1 {
+			s += "\t\t\t},\n"
+		} else {
+			s += "\t\t\t}\n"
+		}
+		str += s
+	}
+	str += "\t\t]"
+	return key, str
 }
 
-func expand_text_json(filed, text string) string {
-	return text
+func expand_json_array(field, text string) (string, string) {
+	text = strings.Trim(text, " ")
+	vals := strings.Split(text, "|")
+
+	if len(vals) == 0 {
+		return field, "[]"
+	}
+
+	str := "[ "
+	for k, val := range vals {
+		str += val
+		if k != len(vals)-1 {
+			str += ", "
+		}
+	}
+	str += "]"
+
+	return field, str
+}
+
+func expand_json_normal(field, text string) (string, string) {
+	return field, strings.Trim(text, " ")
+}
+
+func expand_lua_map(field, text string) (string, string) {
+	text = strings.Trim(text, " ")
+
+	idx := strings.Index(field, "#")
+	if idx == -1 {
+		return field, text
+	}
+
+	field_split := strings.Split(field, "#")
+	key := field_split[0]
+	sub_field := field_split[1]
+
+	if len(text) == 0 {
+		return key, "{}"
+	}
+
+	str := "{\n"
+
+	fields := strings.Split(sub_field, "_")
+
+	values := strings.Split(text, "|")
+	for i, value := range values {
+		sub_values := strings.Split(value, "_")
+		if len(sub_values) != len(fields) {
+			panic("expand_json_map error:" + field + " & " + text)
+		}
+		s := "\t\t\t{\n"
+		for k, sub_value := range sub_values {
+			s = s + "\t\t\t\t" + fields[k] + " = " + sub_value
+			if k != len(sub_values)-1 {
+				s = s + ",\n"
+			} else {
+				s = s + "\n"
+			}
+		}
+		if i != len(values)-1 {
+			s += "\t\t\t},\n"
+		} else {
+			s += "\t\t\t}\n"
+		}
+		str += s
+	}
+	str += "\t\t}"
+	return key, str
+}
+
+func expand_lua_array(field, text string) (string, string) {
+	text = strings.Trim(text, " ")
+	vals := strings.Split(text, "|")
+
+	if len(vals) == 0 {
+		return field, "{}"
+	}
+
+	str := "{ "
+	for k, val := range vals {
+		str += val
+		if k != len(vals)-1 {
+			str += ", "
+		}
+	}
+	str += "}"
+
+	return field, str
+}
+
+func expand_lua_normal(field, text string) (string, string) {
+	return field, strings.Trim(text, " ")
 }
 
 func main() {
@@ -50,19 +184,19 @@ func main() {
 	// sides
 	row = sheet.Rows[1]
 	for i := 0; i < cols; i++ {
-		sides[i] = row.Cells[i].Value
+		sides[i] = strings.Trim(row.Cells[i].Value, "")
 	}
 
 	// field
 	row = sheet.Rows[2]
 	for i := 0; i < cols; i++ {
-		field[i] = row.Cells[i].Value
+		field[i] = strings.Trim(row.Cells[i].Value, "")
 	}
 
 	// types
 	row = sheet.Rows[3]
 	for i := 0; i < cols; i++ {
-		types[i] = row.Cells[i].Value
+		types[i] = strings.Trim(row.Cells[i].Value, "")
 	}
 
 	w := &sync.WaitGroup{}
@@ -87,25 +221,46 @@ func main() {
 		defer f.Close()
 
 		f.WriteString("[\n")
+
+		jcnt := 0
+		for _, v := range sides {
+			if strings.Contains(v, "j") {
+				jcnt++
+			}
+		}
+
 		for i := 4; i < rows; i++ {
 			row := sheet.Rows[i]
 			f.WriteString("\t{\n")
-			l := len(row.Cells) - 1
+			idx := 0
 			for k, cell := range row.Cells {
-				f.WriteString("\t\t\"" + field[k] + "\" : ")
-				text := expand_text_json(field[k], cell.String())
+				if !strings.Contains(sides[k], "j") {
+					continue
+				}
+
+				idx++
+				var key, text string
+				if types[k] == "map" {
+					key, text = expand_json_map(field[k], cell.String())
+				} else if types[k] == "array" {
+					key, text = expand_json_array(field[k], cell.String())
+				} else {
+					key, text = expand_json_normal(field[k], cell.String())
+				}
+
+				f.WriteString("\t\t\"" + key + "\" : ")
 				if types[k] == "string" {
 					f.WriteString("\"" + text + "\"")
 				} else {
 					f.WriteString(text)
 				}
-				if k == l {
+
+				if jcnt == idx {
 					f.WriteString("\n")
 				} else {
 					f.WriteString(",\n")
 				}
 			}
-
 			if i != rows-1 {
 				f.WriteString("\t},\n\n")
 			} else {
@@ -126,7 +281,7 @@ func main() {
 		if err == nil {
 			fmt.Println(file_name + " Checking: OK")
 		} else {
-			fmt.Println(file_name+" Checking: Invalid & ", err)
+			fmt.Println(file_name+" Checking Invalid: ", err)
 		}
 	}()
 
@@ -158,8 +313,20 @@ func main() {
 			f.WriteString(key)
 
 			for k, cell := range row.Cells {
-				f.WriteString("\t\t" + field[k] + " = ")
-				text := expand_text_json(field[k], cell.String())
+				if !strings.Contains(sides[k], "l") {
+					continue
+				}
+
+				var key, text string
+				if types[k] == "map" {
+					key, text = expand_lua_map(field[k], cell.String())
+				} else if types[k] == "array" {
+					key, text = expand_lua_array(field[k], cell.String())
+				} else {
+					key, text = expand_lua_normal(field[k], cell.String())
+				}
+
+				f.WriteString("\t\t" + key + " = ")
 				if types[k] == "string" {
 					f.WriteString("\"" + text + "\"")
 				} else {
