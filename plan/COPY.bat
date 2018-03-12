0a00252eaebce1ba106a10c04214729657d10fd8
preboy.sl <preboy@126.com>
2018-03-12 11:36:45 +0800

    * code tify

--- plan/COPY.bat
+++ plan/COPY.bat
@@ -1,15 +1,15 @@
 @echo off
 
 
-rmdir /S /Q ..\..\2dgame\simulator\win32\src\config
+rmdir /S /Q ..\..\2dgame\simulator\win32\src\configs
 rmdir /S /Q ..\bin\config
 
 
-mkdir ..\..\2dgame\simulator\win32\src\config
+mkdir ..\..\2dgame\simulator\win32\src\configs
 mkdir ..\bin\config
 
 
-copy /Y lua\*.lua   ..\..\2dgame\simulator\win32\src\config\*.lua
+copy /Y lua\*.lua   ..\..\2dgame\simulator\win32\src\configs\*.lua
 copy /Y json\*.json ..\bin\config\*.json
 
 
