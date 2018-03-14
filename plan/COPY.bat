ccbfee0a38406b9827eeb1fcfd399ccfa5958fde
Preboy.ZHANG <preboy@126.com>
2018-03-14 22:19:08 +0800

    config file path changed

--- plan/COPY.bat
+++ plan/COPY.bat
@@ -1,15 +1,15 @@
 @echo off
 
 
-rmdir /S /Q ..\..\2dgame\simulator\win32\src\configs
+rmdir /S /Q ..\..\2dgame\simulator\win32\src\configs_raw
 rmdir /S /Q ..\bin\config
 
 
-mkdir ..\..\2dgame\simulator\win32\src\configs
+mkdir ..\..\2dgame\simulator\win32\src\configs_raw
 mkdir ..\bin\config
 
 
-copy /Y lua\*.lua   ..\..\2dgame\simulator\win32\src\configs\*.lua
+copy /Y lua\*.lua   ..\..\2dgame\simulator\win32\src\configs_raw\*.lua
 copy /Y json\*.json ..\bin\config\*.json
 
 
