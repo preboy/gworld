4b425605eb9ee8528990e888af919d61c8ec5c14
preboy.sl <preboy@126.com>
2018-03-26 20:42:48 +0800

    * upgrada tools

--- plan/ALL.bat
+++ plan/ALL.bat
@@ -8,8 +8,8 @@ del /S /Q lua\*.*
 rem: record export
 exporter jl excel/hero_英雄表.xlsx  HeroProto
 exporter jl excel/creature_怪物表.xlsx  CreatureProto
-exporter jl excel/skill_技能表.xlsx  SkillProto 
-exporter jl excel/aura_光环表.xlsx  AuraProto 
+exporter jl excel/skill_技能表.xlsx  SkillProto
+exporter jl excel/aura_光环表.xlsx  AuraProto
 exporter jl excel/item_道具表.xlsx  ItemProto
 exporter jl excel/creature_team_怪物队伍表.xlsx  CreatureTeam
 
