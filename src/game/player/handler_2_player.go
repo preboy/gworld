85fe3ef61a0dc2b0abbeeb3f5f0a4d11f4e0c7ce
zcg <preboy@126.com>
2019-05-26 20:20:25 +0800

    gm for telent

--- src/game/player/handler_2_player.go
+++ src/game/player/handler_2_player.go
@@ -1,12 +1,10 @@
 package player
 
 import (
-	"fmt"
 	"strings"
 
 	"core/log"
 	"core/tcp"
-	"core/utils"
 	"game/app"
 	"game/config"
 	"game/constant"
@@ -125,51 +123,4 @@ func handler_MarketBuyRequest(plr *Player, packet *tcp.Packet) {
 	}()
 
 	plr.SendPacket(protocol.MSG_SC_MarketBuyResponse, res)
-}
-
-// ----------------------------------------------------------------------------
-
-func (self *Player) on_gm_command(args []string) int32 {
-	fmt.Println("on_gm_command:", args)
-	switch args[0] {
-	case "save":
-		self.Save()
-	case "vip":
-		if len(args) > 1 {
-			val := utils.Atou32(args[1])
-			self.data.Vip = val
-			self.SendNotice("VipLevel: "+utils.U32toa(val), 0)
-		}
-	case "lv":
-		if len(args) > 1 {
-			val := utils.Atou32(args[1])
-			self.data.Lv = val
-			self.SendNotice("Level: "+utils.U32toa(val), 0)
-		}
-	case "item":
-		for i := 1; i < len(args); i++ {
-			ip := app.NewItemProxy(constant.ItemLog_GM)
-			item := strings.Split(args[i], "|")
-			id := utils.Atou32(item[0])
-			ct := utils.Atou32(item[1])
-			ip.Add(id, uint64(ct))
-			ip.Apply(self)
-			println("curr:", item[0], self.GetItem(id))
-		}
-	case "hero":
-		for i := 1; i < len(args); i++ {
-			item := strings.Split(args[i], "|")
-			id := utils.Atou32(item[0])
-			hero := self.GetHero(id)
-			if hero == nil {
-				self.AddHero(id)
-			} else {
-				self.SendNotice("Hero: "+item[0]+" already exist", 0)
-			}
-		}
-	default:
-		println("unknown command:", args[0])
-		return 0
-	}
-	return 1
 }
---
+++ src/game/player/player_gm.go
@@ -0,0 +1,72 @@
+package player
+
+import (
+	"fmt"
+	"strings"
+
+	"core/utils"
+	"game/app"
+	"game/constant"
+)
+
+// ----------------------------------------------------------------------------
+
+func (self *Player) on_gm_command(args []string) int32 {
+	fmt.Println("on_gm_command:", args)
+
+	switch args[0] {
+
+	case "save":
+		self.Save()
+
+	case "vip":
+		if len(args) > 1 {
+			val := utils.Atou32(args[1])
+			self.data.Vip = val
+			self.SendNotice("VipLevel: "+utils.U32toa(val), 0)
+		}
+
+	case "lv":
+		if len(args) > 1 {
+			val := utils.Atou32(args[1])
+			self.data.Lv = val
+			self.SendNotice("Level: "+utils.U32toa(val), 0)
+		}
+
+	case "item":
+		for i := 1; i < len(args); i++ {
+			ip := app.NewItemProxy(constant.ItemLog_GM)
+			item := strings.Split(args[i], "|")
+			id := utils.Atou32(item[0])
+			ct := utils.Atou32(item[1])
+			ip.Add(id, uint64(ct))
+			ip.Apply(self)
+			println("curr:", item[0], self.GetItem(id))
+		}
+
+	case "hero":
+		for i := 1; i < len(args); i++ {
+			item := strings.Split(args[i], "|")
+			id := utils.Atou32(item[0])
+			hero := self.GetHero(id)
+			if hero == nil {
+				self.AddHero(id)
+			} else {
+				self.SendNotice("Hero: "+item[0]+" already exist", 0)
+			}
+		}
+
+	case "prop":
+		str_props := ""
+		for _, hero := range self.data.Heros {
+			str := utils.ObjectToStringPretty(hero.ToBattleUnit()) + "\n\n"
+			str_props += str
+		}
+		self.SendNotice(str_props, 0)
+
+	default:
+		println("unknown command:", args[0])
+		return 0
+	}
+	return 1
+}
