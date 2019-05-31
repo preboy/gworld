7ce5a81a18f5eb525509757d88a9446d21d80660
weideduo <wdd817@gmail.com>
2019-05-31 23:11:00 +0800

    svr info

--- bin/pull/routes/svr_list.js
+++ bin/pull/routes/svr_list.js
@@ -6,36 +6,62 @@ const config = require('../../config.json')
 
 // ----------------------------------------------------------------------------
 
-let server_list = {}
+let svr_list = {}
 
 // ----------------------------------------------------------------------------
 
 function load_server_list() {
     for (let svr in config.games) {
         let tab = config.games[svr]
-        server_list[svr] = {
+        svr_list[svr] = {
             ip:     tab.host,
             svr:    svr,
             port:   tab.port,
-            name:   svr + "'s name",
+            name:   svr,
+            stat:   0,      // 0:维护  1:正常  2:火热  3:新服
         }
     }
 }
 
+
+// id name stat desc
+
+function load_server_stat() {
+    let db = dbmgr.get('c').db();
+    db.collection('server_info').find(cond).toArray((err, docs) => {
+        if (err) {
+            return;
+        }
+
+        for (let i = 0; i < docs.length; i++) {
+
+            let doc = docs[i];
+            let svr = doc.id;
+
+            if (svr_list[svr]) {
+                svr_list[svr].name = doc.name;
+                svr_list[svr].stat = doc.stat;
+            }
+        }
+    });
+}
+
+
 function init() {
     load_server_list()
+    load_server_stat()
 }
 
 // ----------------------------------------------------------------------------
 
 router.get("/reload", function(req, res) {
-    server_list = {}
+    svr_list = {}
     load_server_list()
 })
 
 
 router.get("/", function(req, res) {
-    res.json(server_list);
+    res.json(svr_list);
 });
 
 
