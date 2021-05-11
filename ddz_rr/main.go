632d642cb74c6a2ec1365bc93c255e0e58fd76d6
zhangcg <zhangcg@mail.jj.cn>
2021-05-11 18:14:47 +0800

    ddz_rr

--- ddz_rr/main.go
+++ ddz_rr/main.go
@@ -1,9 +1,50 @@
 package main
 
+import (
+	"gworld/core/loop"
+	"gworld/core/tcp"
+	"gworld/ddz/config"
+	"gworld/game/loop"
+)
+
+type session struct {
+}
+
+func (self *session) OnRecvPacket(packet *tcp.Packet) {
+
+}
+func (self *session) OnOpened() {
+}
+func (self *session) OnClosed() {
+}
+
+func (self *session) SendPacket(opcode uint16, data []byte) {
+
+}
+
+var (
+	quit = make(chan bool)
+)
+
 func main() {
+	if err := config.Load(); err != nil {
+		panic(err)
+	}
 
-	// send
+	loop.Start()
 
-	// process
+	sess := &session{}
+
+	{
+		conn, err := tcp.Connect(config.Get().Addr4Referee)
+		if err != nil {
+			panic(err)
+		}
+
+		sock := tcp.NewSocket(conn, sess)
+		sock.Start()
+	}
+
+	<-quit
 
 }
