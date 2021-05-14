c8fccdad260155f73a6078b242727f59c8fd89d5
zhangcg <zhangcg@mail.jj.cn>
2021-05-14 13:47:40 +0800

    move bin

--- ddz_build.sh
+++ ddz_build.sh
@@ -7,13 +7,13 @@ echo "Installing DDZ for '$platform' ..."
 
 if [ $platform = "windows" ];
 then
-    go build -o bin/ddz.exe     -gcflags="-N -l" ddz/main.go
-    go build -o bin/ddz_ai.exe  -gcflags="-N -l" ddz_ai/main.go
-    go build -o bin/ddz_rr.exe  -gcflags="-N -l" ddz_rr/main.go
+    go build -o bin_ddz/ddz.exe     -gcflags="-N -l" ddz/main.go
+    go build -o bin_ddz/ddz_ai.exe  -gcflags="-N -l" ddz_ai/main.go
+    go build -o bin_ddz/ddz_rr.exe  -gcflags="-N -l" ddz_rr/main.go
 else
-    go build -o bin/ddz         -gcflags="-N -l" ddz/main.go
-    go build -o bin/ddz_ai      -gcflags="-N -l" ddz_ai/main.go
-    go build -o bin/ddz_rr      -gcflags="-N -l" ddz_rr/main.go
+    go build -o bin_ddz/ddz         -gcflags="-N -l" ddz/main.go
+    go build -o bin_ddz/ddz_ai      -gcflags="-N -l" ddz_ai/main.go
+    go build -o bin_ddz/ddz_rr      -gcflags="-N -l" ddz_rr/main.go
 fi;
 
 
